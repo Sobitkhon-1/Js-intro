99b112850c7057594dd3be6d62b3b733f03fa879
Sobitkhon Khoshimov <ksobitkhon.@gmail.com>
2025-04-20 22:08:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+ // input a and b
+ const a = Number(prompt("Enter value for a:"));
+ const b = Number(prompt("Enter value for b:"));
+
+ // Calculate a * b / 2
+ const result = (a * b) / 2;
+
+ // Show result in console
+ console.log(`a = ${a}`);
+ console.log(`b = ${b}`);
+ console.log(`(${a} * ${b}) / 2 = ${result}`);
